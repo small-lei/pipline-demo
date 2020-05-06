9db53eefd2eddf25ee28ee3b3c908e0683a04181
small-lei <358769548@qq.com>
2020-05-06 22:06:34 +0800

    修改
    Signed-off-by: small-lei <358769548@qq.com>

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ func main() {
 	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI！")
 	fmt.Println("hello, welcome zhangxiaolei image!")
 	fmt.Println("来了老弟！！！！")
-	fmt.Println("修改一下！")
+	fmt.Println("修改一下今天！")
 
 	//1、一次性读取文件内容,还有一个 ReadAll的函数，也能读取
 	data, err := ioutil.ReadFile("./conf/config.ini")
